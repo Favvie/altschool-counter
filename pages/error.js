e8c8b19ef8cb12089af5d5c3a1dd6b7b49e286b2
Favvie <66237754+Favvie@users.noreply.github.com>
2022-11-08 05:12:05 +0100

    error boundary update

--- pages/error.js
+++ pages/error.js
@@ -1,14 +1,21 @@
 import styles from '../styles/Home.module.css';
+import { useState } from 'react';
+
+const CrashableComponent = (props) => {
+  return <span>{props.iDontExist.prop}</span>;
+};
 
 const ErrorPage = () => {
+  const [error, setError] = useState(false);
   const handleClick = () => {
-    throw new Error('testing the error boudnary');
+    setError(true);
   };
   return (
     <div>
       <button onClick={handleClick} className={styles.notfoundBtn}>
         Click me
       </button>
+      {error && <CrashableComponent />}
     </div>
   );
 };
